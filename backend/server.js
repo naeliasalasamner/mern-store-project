f8759cbb732476e4fa8bfa56dcc12a3ad0fb426c
Naelia Salas <naeliasalas@gmail.com>
2024-11-05 17:07:24 +0100

    Adjusting server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Connect to MongoDB (use your MongoDB URI from .env)
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
