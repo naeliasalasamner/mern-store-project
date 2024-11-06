6fecd714a3fc60570281cb559e35a7c09efc5822
Naelia Salas <naeliasalas@gmail.com>
2024-11-06 06:41:45 +0100

    Updating server.js

--- backend/server.js
+++ backend/server.js
@@ -25,6 +25,7 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'));
   });
 }
+console.log("Current Directory:", __dirname);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
